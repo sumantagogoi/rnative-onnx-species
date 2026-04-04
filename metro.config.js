b894f812ddecb4e78507cd219700bd979bb7edd8
sumanta_xynocast <sumanta@xynocast.com>
2026-04-04 03:02:19 +0300

    Fix Metro config for labels.txt

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('onnx');
+config.resolver.assetExts.push('onnx', 'txt');
 
 module.exports = config;
